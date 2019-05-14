f9e9c0ae559c6c6c16745eb412f446f0aa7150d7
Cezanne <cezannec@alumni.stanford.edu>
2019-05-13 20:58:22 -0700

    convert input data to float and add comments

--- project-ml-microservice-kubernetes/app.py
+++ project-ml-microservice-kubernetes/app.py
@@ -12,10 +12,10 @@ LOG.setLevel(logging.INFO)
 
 def scale(payload):
     """Scales Payload"""
-
-    LOG.info(f"Scaling Payload: {payload}")
-    scaler = StandardScaler().fit(payload)
-    scaled_adhoc_predict = scaler.transform(payload)
+    
+    LOG.info(f"Scaling Payload: \n{payload}")
+    scaler = StandardScaler().fit(payload.astype(float))
+    scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
 
 @app.route("/")
@@ -26,42 +26,46 @@ def home():
 @app.route("/predict", methods=['POST'])
 def predict():
     """Performs an sklearn prediction
-
-    input looks like:
-            {
-    "CHAS":{
-      "0":0
-    },
-    "RM":{
-      "0":6.575
-    },
-    "TAX":{
-      "0":296.0
-    },
-    "PTRATIO":{
-       "0":15.3
-    },
-    "B":{
-       "0":396.9
-    },
-    "LSTAT":{
-       "0":4.98
-    }
-
-    result looks like:
-    { "prediction": [ <val> ] }
-
-    """
-
-    # TO DO:  Log the output prediction value
+        
+        input looks like:
+        {
+        "CHAS":{
+        "0":0
+        },
+        "RM":{
+        "0":6.575
+        },
+        "TAX":{
+        "0":296.0
+        },
+        "PTRATIO":{
+        "0":15.3
+        },
+        "B":{
+        "0":396.9
+        },
+        "LSTAT":{
+        "0":4.98
+        }
+        
+        result looks like:
+        { "prediction": [ <val> ] }
+        
+        """
+    
+    # Logging the input payload
     json_payload = request.json
-    LOG.info(f"JSON payload: {json_payload}")
+    LOG.info(f"JSON payload: \n{json_payload}")
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info(f"inference payload DataFrame: {inference_payload}")
+    LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
+    # scale the input
     scaled_payload = scale(inference_payload)
+    # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
+    # TO DO:  Log the output prediction value
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
+    # load pretrained model as clf
     clf = joblib.load("./model_data/boston_housing_prediction.joblib")
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
