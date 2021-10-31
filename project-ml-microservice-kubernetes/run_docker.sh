0c61d9695a661e0e677394baaf0c1d1c5e335d2d
SwapnaGondi <88293116+SwapnaGondi@users.noreply.github.com>
2021-10-31 22:33:03 +0000

    Update run_docker.sh

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -5,8 +5,10 @@
 # Step 1:
 # Build image and add a descriptive tag
 
+docker build --tag=udacityproj4 .
 # Step 2: 
 # List docker images
-
+docker image ls
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 udacityproj4
