e22db2039829ace2467201607315e144db92f1d7
acholonu <ucacholonu@hotmail.com>
2021-11-01 23:06:12 -0500

    Update ports for docker build step

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,6 +4,8 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+# Docker images from a Dockerfile and a “context”. A build’s context is the set of files located in the specified PATH or URL. 
+# The Docker build process can access any of the files located in this context.
 docker build --tag=housingapp .
 
 # Step 2: 
@@ -13,4 +15,4 @@ docker image ls
 # Step 3: 
 # Run flask app
 # docker run -p <container port>:<host port>
-docker run -p 8000:5001 housingapp
+docker run -p 8000:80 housingapp
