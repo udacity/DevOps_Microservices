2ec3f3de5fdce4f1a7f4df78531d7c28b150c509
SudKul <48475411+SudKul@users.noreply.github.com>
2021-11-06 22:56:04 +0530

    Update wikipedia_lambda_solution.py
    Add exception handling

--- Lesson-1-Lambda-functions/wikipedia-query/wikipedia_lambda_solution.py
+++ Lesson-1-Lambda-functions/wikipedia-query/wikipedia_lambda_solution.py
@@ -17,7 +17,22 @@ def lambda_handler(event, context):
     
     # get the wikipedia "entity" from the body of the request
     entity = event["entity"]
-    res = wikipedia.summary(entity, sentences=1) # first sentence, result
+    # res = wikipedia.summary(entity, sentences=1) # first sentence, result
+    BAD_REQUEST_STATUS = 400
+    ALL_GOOD_STATUS = 200 
+    # Exception handling
+    try:
+        res = wikipedia.summary(entity, sentences=1) # first sentence, result
+        statusCode = ALL_GOOD_STATUS
+    except wikipedia.exceptions.PageError:
+        res= "\nThis word does not exist!\n"
+        statusCode = BAD_REQUEST_STATUS
+    except wikipedia.exceptions.DisambiguationError: 
+        statusCode = BAD_REQUEST_STATUS
+        res = "\nThere are multiple references to this word!\n"
+    except:
+        statusCode = BAD_REQUEST_STATUS
+        res = "\nSorry, Cannot Handle this request!\n"
 
     # print statements
     print(f"context: {context}, event: {event}")
@@ -25,7 +40,7 @@ def lambda_handler(event, context):
     
     # format the response as JSON and return the result
     response = {
-        "statusCode": "200", 
+        "statusCode": statusCode, 
         "headers": { "Content-type": "application/json" },
         "body": json.dumps({"message": res})
     }
