3bc865431e7d6d5a01e335c6a2cef0e21eb10906
EC2 Default User <ec2-user@ip-172-31-7-104.ec2.internal>
2022-04-12 03:45:58 +0000

    Implement build list and run commands

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=ml-microservice .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -it --publish 3000:80 ml-microservice
