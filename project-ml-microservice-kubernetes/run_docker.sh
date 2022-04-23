662457dfb8c6ae6747de9f803c651a6e447f5dc2
Ricardo Bonilla Morales <ricardo.bonilla.morales@gmail.com>
2022-04-23 00:44:12 +0000

    Upload docker image to remote docker hub repository

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=ml-microservice .
+docker build --tag=richbm10/my-private-repo:ml-microservice .
 
 # Step 2: 
 # List docker images
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+# https://docs.docker.com/docker-hub/repos/#:~:text=To%20push%20an%20image%20to,docs%2Fbase%3Atesting%20).
+dockerpath=richbm10/my-private-repo
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+cat ~/my_password.txt | docker login --username richbm10 --password-stdin
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:ml-microservice
