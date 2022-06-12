9fc0a8a51a47c6d08d3a08a02df5a6e867ba55fd
Bhuvanhruday <70108798+Bhuvanhruday@users.noreply.github.com>
2022-06-13 00:14:31 +0530

    Added via webbrowser

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
-
+tag=project4-docker-udacity
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag $tag .
 
 # Step 2: 
 # List docker images
-
+docker image ls
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 $tag
--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+ dockerpath=project4-docker-udacity
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run project4-docker-udacity  --image=bmailazhagan/$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward project4-docker-udacity 8000:80
 
