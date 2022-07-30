11e6bfd15997dfcc2382b2e55f0f69f0b353e660
EC2 Default User <ec2-user@ip-172-31-7-221.us-west-1.compute.internal>
2022-07-30 22:09:59 +0000

    kubernetes_out and run_kubernetes added

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -3,16 +3,18 @@
 # This tags and uploads an image to Docker Hub
 
 # Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
+dockerpath="ayo32/project4:latest"
 
 # Step 2
-# Run the Docker Hub container with kubernetes
+kubectl run udacityproject\
+#   --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=udacityproject
 
 
 # Step 3:
-# List kubernetes pods
+kubectl get pods
 
 # Step 4:
-# Forward the container port to a host
+kubectl port-forward udacityproject 8000:80
 
