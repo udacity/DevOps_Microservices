2874ec044ad43ff0e905f122068956a5fe5d1e98
EC2 Default User <ec2-user@ip-172-31-7-221.us-west-1.compute.internal>
2022-07-30 03:50:00 +0000

    update files to account

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -1,12 +1,10 @@
-#!/usr/bin/env bash
-
-## Complete the following steps to get Docker running locally
+!/usr/bin/env bash
 
 # Step 1:
-# Build image and add a descriptive tag
+sudo docker build -t udacity .
 
 # Step 2: 
-# List docker images
+docker image ls
 
 # Step 3: 
-# Run flask app
+sudo docker run -p 8000:80 udacity 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -1,15 +1,13 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="ayo32/project4:latest"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
-# Push image to a docker repository
+docker image push $dockerpath
