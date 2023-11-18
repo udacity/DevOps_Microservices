fa12ee7e933dd689f4aabeca3fa8a7157e30f466
lx96 <lamdo0612@gmail.com>
2023-11-18 17:55:01 +0000

    submmit

--- project-ml-microservice-kubernetes/app.py
+++ project-ml-microservice-kubernetes/app.py
@@ -1,4 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import (
+    Flask,
+    request,
+    jsonify
+    )
 from flask.logging import create_logger
 import logging
 
@@ -63,6 +67,7 @@ def predict():
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
+    LOG.info(f'prediction value: {prediction}')
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
